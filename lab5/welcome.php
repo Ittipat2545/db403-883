b38d64936760d27e514ab9be3929bc9c98aecc4a
Ittipat2545 <65110826@dpu.ac.th>
2022-10-16 17:31:35 +0700

    day3

---
+++ lab5/welcome.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p> fname : <?=$_POST['fname']?></P>
+    <p> lname : <?=$_POST['lname']?></P>
+    <p> gemder: <?=$_POST['gender']?></P>
+    <p> dob : <?=$_POST['dob']?></P>
+    <p> email : <?=$_POST['email']?></P>
+    <p> password : <?=$_POST['password']?></P>
+</body>
+</html>
